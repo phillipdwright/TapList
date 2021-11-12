bc41acba309cdc44a40725a7af5108b77c51e3eb
Phil Wright <wright.phillip@gmail.com>
2021-11-12 12:33:24 -0700

    Add navigator with Home and Detail screens

---
+++ components/Detail.js
@@ -0,0 +1,11 @@
+import { View } from 'react-native';
+
+const Detail = () => {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Details Screen</Text>
+      </View>
+    );
+  }
+
+export default Detail;
